af455313ab13cdc748d4efce8a86b9686314308e
Нодир Асомов <nasomov93@gmail.com>
2023-10-15 17:35:32 +0500

    hi

--- src/main/java/lang/print/gaps/task5/NumberDivider.java
+++ src/main/java/lang/print/gaps/task5/NumberDivider.java
@@ -2,5 +2,12 @@ package lang.print.gaps.task5;
 
 public class NumberDivider {
     void divide(int toBeDivided) {
+        System.out.println(toBeDivided/5);
+
+    }
+
+    public static void main(String[] args) {
+        NumberDivider obj= new NumberDivider();
+        obj.divide(50);
     }
 }
--- src/main/java/lang/print/gaps/task5/TimeConvertor.java
+++ src/main/java/lang/print/gaps/task5/TimeConvertor.java
@@ -1,6 +1,15 @@
 package lang.print.gaps.task5;
 
 public class TimeConvertor {
+    //float second;
     public void convert(float minutes) {
+        System.out.println(minutes *60);
+    }
+
+    public static void main(String[] args) {
+        TimeConvertor obj = new TimeConvertor();
+        obj.convert(3);
     }
 }
+
+
